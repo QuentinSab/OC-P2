48a3fac2615cdb359d9153e3c93e61dd054f54d6
QuentinSab <quentinsab@hotmail.fr>
2024-11-11 15:49:22 +0100

    updated format to not exceed maximum line lengths and cleaned up comments

--- functions.py
+++ functions.py
@@ -7,9 +7,10 @@ from bs4 import BeautifulSoup
 
 
 def extract_upc(soup, book_data):
-    """Extracts the UPC (Universal Product Code) and appends it to book data."""
+    """
+    Extracts the UPC (Universal Product Code) and appends it to book data.
+    """
 
-    # Find all table data cells
     td_data = soup.find_all("td")
     book_data.append(td_data[0].text)
 
@@ -17,7 +18,6 @@ def extract_upc(soup, book_data):
 def extract_title(soup, book_data):
     """Extracts the book title and appends it to book data."""
 
-    # Find the first <h1> tag which contains the book title
     h1_title = soup.find("h1")
     book_data.append(h1_title.text)
 
@@ -25,9 +25,8 @@ def extract_title(soup, book_data):
 def extract_price_incl_tax(soup, book_data):
     """Extracts the price including tax and appends it to book data."""
 
-    # Find all table data cells
     td_data = soup.find_all("td")
-    # Extract price including tax from the fourth cell and remove the currency symbol
+    # Extract price from the fourth cell and remove the currency symbol
     price_incl_tax = td_data[3].text[1:]
     book_data.append(price_incl_tax)
 
@@ -35,9 +34,8 @@ def extract_price_incl_tax(soup, book_data):
 def extract_price_excl_tax(soup, book_data):
     """Extracts the price excluding tax and appends it to book data."""
 
-    # Find all table data cells
     td_data = soup.find_all("td")
-    # Extract price excluding tax from the third cell and remove the currency symbol
+    # Extract price from the third cell and remove the currency symbol
     price_excl_tax = td_data[2].text[1:]
     book_data.append(price_excl_tax)
 
@@ -45,7 +43,6 @@ def extract_price_excl_tax(soup, book_data):
 def extract_stock(soup, book_data):
     """Extracts the stock quantity and appends it to book data."""
 
-    # Find all table data cells
     td_data = soup.find_all("td")
     # Extract and transform stock information from the appropriate cell
     stock = transform_stock(td_data[5].text)
@@ -55,9 +52,7 @@ def extract_stock(soup, book_data):
 def extract_description(soup, book_data):
     """Extracts the product description and appends it to book data."""
 
-    # Locate the description section
     div_product_description = soup.find("div", class_="sub-header")
-    # Find the description within the next paragraph element
     product_description = div_product_description.find_next("p")
     book_data.append(product_description.text)
 
@@ -65,15 +60,15 @@ def extract_description(soup, book_data):
 def extract_category_name(soup, book_data):
     """Extracts the category name and appends it to book data."""
 
-    anchor = soup.find_all("a")  # Find all anchors
+    anchor = soup.find_all("a")
     book_data.append(anchor[3].text)
 
 
 def extract_rating(soup, book_data):
     """Extracts the book's rating and appends it to book data."""
 
-    p_rating = soup.find("p", class_="star-rating")  # Find the rating element
-    rating = p_rating["class"][1]  # Extract the rating text
+    p_rating = soup.find("p", class_="star-rating")
+    rating = p_rating["class"][1]
     rating = transform_rating(rating)  # Convert text rating to integer
     book_data.append(rating)
 
@@ -81,7 +76,6 @@ def extract_rating(soup, book_data):
 def extract_url_img(soup, book_data, base_url):
     """Extracts the image URL and appends it to book data."""
 
-    # Find the first image tag in the HTML
     path_img = soup.find("img")
     # Returns full cover image URL
     book_data.append(base_url + path_img["src"][5:])
@@ -90,9 +84,9 @@ def extract_url_img(soup, book_data, base_url):
 def transform_stock(text_stock):
     """Extracts the stock quantity as an integer from a given text."""
 
-    # Search for the first sequence of digits in the input string
+    # Search for the first sequence of digits in the string
     stock = search("(\\d+)", text_stock)
-    # Convert the matched digits to an integer an returns it
+    # Convert the digits to an integer and returns it
     return int(stock.group(1))
 
 
@@ -118,15 +112,12 @@ def create_scrap_directory():
     """Creates a timestamped directory for storing scraped data and image."""
 
     current_date = date.today()
-    str_current_date = current_date.strftime("%d-%m-%Y")  # Format date as a string
+    # Converts date in a string
+    str_current_date = current_date.strftime("%d-%m-%Y")
     directory_name = "books_to_scrape_" + str_current_date
 
-    # Check if the directory already exists
     if not os.path.exists(directory_name):
-
-        # Create the directory if it doesn't exist
         os.mkdir(directory_name)
-
         print('Création du dossier "' + directory_name + '"')
 
     return directory_name
@@ -135,25 +126,22 @@ def create_scrap_directory():
 def create_category_directory(category_name, parent_directory):
     """Creates a folder named after the category and returns its path."""
 
-    # Define the path for the category folder
     category_image_path = parent_directory + "/" + category_name + "_image"
 
-    # Check if the folder already exists
     if not os.path.exists(category_image_path):
-
-        # Create the folder if it doesn't exist
         os.mkdir(category_image_path)
 
-    return category_image_path  # Return the path to the new or existing folder
+    return category_image_path
 
 
 def get_page(page_url):
     """Fetches and parses page content from a URL."""
 
     page = requests.get(page_url, timeout=10)
+
     if page.status_code == 200:  # If the request is successful
-        soup = BeautifulSoup(page.content, "html.parser")  # Parse content
-        return soup  # Return parsed content
+        soup = BeautifulSoup(page.content, "html.parser")
+        return soup
 
     print(f"Erreur {page.status_code} lors de la requête de : {page_url}")
 
@@ -161,9 +149,9 @@ def get_page(page_url):
 def get_categories(soup):
     """Fetches the list of categories from a page content"""
 
-    ul_category = soup.find_all("ul")[2]  # Find the third <ul> element in the page
-    li_category = ul_category.find_all("li")  # Extract all <li> elements from the <ul>
-    return li_category  # Return the list of categories as <li> elements
+    ul_category = soup.find_all("ul")[2]
+    li_category = ul_category.find_all("li")
+    return li_category
 
 
 def get_category_name(category_url):
@@ -179,33 +167,38 @@ def get_category_name(category_url):
 def get_book_title(book_url):
     """Fetches the book title from its URL"""
 
-    book_title = book_url.rsplit("/", 2)[1]  # Extract the part before the last "/"
-    book_title = book_title.rsplit("_", 1)[0]  # Extract the part before the last "_"
+    # Extract the part before the last "/"
+    book_title = book_url.rsplit("/", 2)[1]
+    # Extract the part before the last "_"
+    book_title = book_title.rsplit("_", 1)[0]
     return book_title
 
 
 def get_books_url(category_url, base_url):
     """Fetches all books URL from a category and adds them to a list"""
 
-    soup = get_page(category_url)  # Load the category page
+    soup = get_page(category_url)
 
-    books_url = []  # Initialize list to store book URLs
-    category_n_page = 1  # Start with the first page
+    books_url = []
+    category_page_n = 1
 
-    # Fetch books URL from the first page
     get_local_books_url(soup, books_url, base_url)
 
     # Fetch books URL from the subsequent pages
     while soup.find("li", class_="next"):  # Check if there is a next page
-        category_n_page += 1  # Increment page number
+        category_page_n += 1
 
         # Generate the next page URL
         category_url = (
-            category_url.rpartition("/")[0] + "/page-" + str(category_n_page) + ".html"
+            category_url.rpartition("/")[0]
+            + "/page-"
+            + str(category_page_n)
+            + ".html"
         )
 
         soup = get_page(category_url)  # Load the next page
-        get_local_books_url(soup, books_url, base_url)  # Fetch books URL from the page
+
+        get_local_books_url(soup, books_url, base_url)
 
     return books_url
 
@@ -213,7 +206,6 @@ def get_books_url(category_url, base_url):
 def get_local_books_url(soup, books_url, base_url):
     """Fetches books URL from a category page and adds them to a list"""
 
-    for h3 in soup.find_all("h3"):  # Iterate over each book title
-        # Build book URL
+    for h3 in soup.find_all("h3"):
         book_url = base_url + "/catalogue/" + (h3.find("a")["href"][9:])
-        books_url.append(book_url)  # Add the URL to the list
+        books_url.append(book_url)
--- main.py
+++ main.py
@@ -2,44 +2,46 @@
 
 import csv
 import sys
-
 from functions import *
-
 
 BASE_URL = "http://books.toscrape.com"
 
 
 def etl_site(directory_path):
-    """Extracts data and cover images from all categories and store them at the specified path."""
+    """
+    Extracts data and cover images from all categories and stores them
+    at the specified path.
+    """
 
-    soup = get_page(BASE_URL + "/index.html")  # Load the main page
+    soup = get_page(BASE_URL + "/index.html")
 
-    for a_category in get_categories(soup):  # Iterate over each category
+    for a_category in get_categories(soup):
 
-        category_url = (
-            BASE_URL + "/" + a_category.find("a")["href"]  # Build category URL
-        )
-        etl_category(category_url, directory_path)  # Extract data for each category
+        category_url = BASE_URL + "/" + a_category.find("a")["href"]
+        etl_category(category_url, directory_path)
 
 
 def etl_category(category_url, directory_path):
-    """Extracts data and cover images for a category and stores them at the specified path."""
+    """
+    Extracts data and cover images for a category and stores them at
+    the specified path.
+    """
 
-    # Get the category name from URL
     category_name = get_category_name(category_url)
-    # Create directory for images in the specified directory
-    category_image_path = create_category_directory(category_name, directory_path)
-    # Define CSV file path
+
+    category_image_path = create_category_directory(
+        category_name, directory_path
+    )
+
     file_csv_path = directory_path + "/" + category_name + "_data.csv"
 
-    # Extract and save data
     load_data(category_url, file_csv_path, category_image_path)
 
 
 def load_data(url_to_extract, file_csv, image_path):
     """
-    Loads data from a specified URL into a CSV file
-    and downloads cover images to the specified path.
+    Loads data from a specified URL into a CSV file and downloads cover
+    images to the specified path.
     """
 
     with open(file_csv, mode="w", newline="", encoding="utf-8") as fichier:
@@ -58,27 +60,28 @@ def load_data(url_to_extract, file_csv, image_path):
         ]
 
         writer = csv.writer(fichier)
-        writer.writerow(csv_header)  # Write CSV header
+        writer.writerow(csv_header)
 
         # Process if the URL points to a category page
-        if (url_to_extract.rsplit("/", 3)[0]) == (BASE_URL + "/catalogue/category"):
+        if (url_to_extract.rsplit("/", 3)[0]) == (
+            BASE_URL + "/catalogue/category"
+        ):
 
-            # Fetch and iterate over each book URL from the category
             for book_url in get_books_url(url_to_extract, BASE_URL):
-                book_data = extract_book_data(book_url)
-                writer.writerow(book_data)  # Write book data to CSV
 
-                # Download the cover image
+                book_data = extract_book_data(book_url)
+                writer.writerow(book_data)
+
                 download_image(book_url, book_data[9], image_path)
 
             print(f'Catégorie "{book_data[7]}" extraite avec succès.')
 
         # Process if the URL points to a single book page
         else:
+
             book_data = extract_book_data(url_to_extract)
             writer.writerow(book_data)
 
-            # Download the cover image
             download_image(url_to_extract, book_data[9], image_path)
 
             print(f'Livre "{book_data[2]}" extrait avec succès.')
@@ -87,47 +90,50 @@ def load_data(url_to_extract, file_csv, image_path):
 def download_image(book_url, image_url, image_path):
     """Downloads a book cover image and saves it to the specified path."""
 
-    # Fetch image data
     img_data = requests.get(image_url, timeout=10).content
-    # Generate file name from book title
     file_image_name = image_path + "/" + get_book_title(book_url) + ".jpg"
 
-    # Save image data to a file in write mode
     with open(file_image_name, "wb") as img_file:
         img_file.write(img_data)
 
 
 def extract_book_data(book_url):
-    """Extracts and returns a list containing all data for a given book URL."""
+    """
+    Extracts and returns a list containing all data for a given book
+    URL.
+    """
 
-    soup = get_page(book_url)  # Load the page content into a object
+    soup = get_page(book_url)
     book_data = []
 
-    # Append each piece of book data to the list
-    book_data.append(book_url)  # Book URL
-    extract_upc(soup, book_data)  # Universal Product Code (UPC)
-    extract_title(soup, book_data)  # Book title
-    extract_price_incl_tax(soup, book_data)  # Price including tax
-    extract_price_excl_tax(soup, book_data)  # Price excluding tax
-    extract_stock(soup, book_data)  # Stock availability
-    extract_description(soup, book_data)  # Book description
-    extract_category_name(soup, book_data)  # Category name
-    extract_rating(soup, book_data)  # Review rating
-    extract_url_img(soup, book_data, BASE_URL)  # Cover image URL
+    book_data.append(book_url)
+    extract_upc(soup, book_data)
+    extract_title(soup, book_data)
+    extract_price_incl_tax(soup, book_data)
+    extract_price_excl_tax(soup, book_data)
+    extract_stock(soup, book_data)
+    extract_description(soup, book_data)
+    extract_category_name(soup, book_data)
+    extract_rating(soup, book_data)
+    extract_url_img(soup, book_data, BASE_URL)
 
     return book_data
 
 
 def main():
-    """Extracts data based on given system argument: full site, category, or single book."""
+    """
+    Extracts data based on given system argument: full site, category,
+    or single book.
+    """
 
-    # Create directory for extracted data
     scrap_directory = create_scrap_directory()
 
     # Full site extraction if no argument is provided
     if len(sys.argv) < 2:
+
         print(
-            f'Extraction de "{BASE_URL}" dans le dossier "{scrap_directory}" en cours ...'
+            f'Extraction de "{BASE_URL}" dans le dossier "{scrap_directory}" '
+            "en cours ..."
         )
         etl_site(scrap_directory)
         sys.exit(1)
@@ -136,7 +142,10 @@ def main():
     elif len(sys.argv) == 2:
 
         # Extract a category if the system argument is a category URL
-        if (sys.argv[1].rsplit("/", 3)[0]) == (BASE_URL + "/catalogue/category"):
+        if (sys.argv[1].rsplit("/", 3)[0]) == (
+            BASE_URL + "/catalogue/category"
+            ):
+
             print(
                 f'Extraction de "{sys.argv[1]}" dans '
                 f'le dossier "{scrap_directory}" en cours ...'
